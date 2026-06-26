b4650c54b21681b7618dcad268062dc4baa6c8e5
yuu3232 <45nosekaihe1371@gmail.com>
2026-06-26 20:45:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
     alert("ボタンが押された！");
 });
+const now = new Date();
+console.log("アクセス時間:", now.toLocaleString());
+
+const btn = document.getElementById("btn");
+
+navigator.geolocation.getCurrentPosition(
+    function(position){
+        console.log("緯度:", position.coords.latitude);
+        console.log("経度:", position.coords.longitude);
+    },
+    function(error){
+        console.log("位置情報は取得できませんでした。");
+    }
+);
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    alert("ボタンが押された！");
+});
